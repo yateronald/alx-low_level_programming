f82ae0f581e15d13f19c7339377e4a1083709506
yateronald <yateronald@gmail.com>
2022-03-10 14:17:17 -0800

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,8 @@
 
 int main(void)
 {
-  char strg[]="\"Programming is like building a multilingual puzzle";
-  puts(strg);
-  return 0;
+char strg[] = "\"Programming is like building a multilingual puzzle";
+puts(strg);
+return (0);
 }
+/*This is the used of Puts function inside sting.h*/
