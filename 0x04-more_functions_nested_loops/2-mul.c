8293bbb039d3b9a4bcf023c97ab919b71fab4d52
yateronald <yateronald@gmail.com>
2022-03-17 13:47:05 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <main.h>
+
+/**
+ * mul - multiplication code
+ *
+ * @a: first input
+ * @b: second input
+ * return value
+ */
+
+int mul(int a, int b)
+{
+int mult;
+mult = a * b;
+return (mult);
+}
