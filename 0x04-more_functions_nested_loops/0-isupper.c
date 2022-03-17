72d14d57171044329982c491e09affd30a33d68d
yateronald <yateronald@gmail.com>
2022-03-17 12:34:57 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,16 @@
  *
  * Return: Always 0.
  */
+int main(void)
+{
+    char c;
 
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    c = 'a';
+    printf("%c: %d\n", c, _isupper(c));
+    return (0);
+}
 int _isupper(int  c)
 {
 if (c <= 'A' && c >= 'A')
