9ca0ad6afe847ddf7c36cb500ac02705ca0195d3
yateronald <yateronald@gmail.com>
2022-03-17 14:42:03 -0700

    isdigit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,24 +3,18 @@
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
  *
- * Return: 0-9 followed by newline
+ * Return: 0-9, excluding 2 and 4, followed by new line
  */
-
 void print_most_numbers(void)
 {
-int i;
-for (i = 0; i <= 9 ; i++)
-{
-if ((i != 2) && (i != 4))
-{
-_putchar(i + '0');
-}
-}
-_putchar('\n');
-}
+	int i;
 
-int main(void)
-{
-    print_most_numbers();
-    return (0);
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -4,3 +4,7 @@ int mul(int a,int b);
 void print_numbers(void);
 int _putchar(int c);
 void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
