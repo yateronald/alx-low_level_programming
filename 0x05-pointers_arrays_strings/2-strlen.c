2462091227231dc243edca25c3510c50c9031817
yateronald <yateronald@gmail.com>
2022-03-21 14:54:14 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen  - char as input
+ * @s :char variable
+ * Return: Always 0.
+ */
+
+int _strlen(char *s)
+{
+int i = 0;
+for (i = 0; s[i] != '\0'; ++i);
+{
+i = i;
+}
+return (i);
+}
