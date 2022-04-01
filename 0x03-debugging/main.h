7fa78bdc5f5f19662692d1cf16cf5d1f2e81805d
yateronald <39405940+yateronald@users.noreply.github.com>
2022-04-02 04:06:31 +0530

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,3 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
 
 #include <stdio.h>
 
@@ -8,4 +6,3 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* HOLBERTON_H */
