c33ee761a3dba1a3c7dcbfa1e9ae9b07f963a6cf
yateronald <yateronald@gmail.com>
2022-04-01 15:18:59 -0700

    my commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * main - prints its name + \n
+ * @argc: count number of argument
+ * @argv: olding all the
+ * Return: 0
+ */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-  printf ("%s\n",argv[0]);
-  return 0;
+printf("%s\n", argv[0]);
+return (0);
 }
